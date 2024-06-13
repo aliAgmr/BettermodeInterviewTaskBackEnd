403936f18bc77da7ea02028f7403236085f47414
Ali Aghamiri <www.aliaghamiry@gmail.com>
2024-06-13 20:11:29 +0100

    Remove deploy files

--- deploy-files/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rsync -avzhP --delete ~/Developer/bettermode/interview-task-backend/ root@82.102.10.111:~/bettermode/interview-task-backend
-
-ssh root@82.102.10.111 'cd ~/bettermode/interview-task-backend/deploy-files; docker compose down; docker compose up -d --build; docker compose logs;'
